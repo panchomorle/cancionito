67e6b04268f9b770d06d3d09e26e048a05f8f3ad
Juan Pablo Morales <eljuampimorales@gmail.com>
2024-04-29 23:22:37 -0300

    app funcional sin bdd
    Extraje el manejo de característica a la funcion filtrar_number
    Creé la funcionalidad para verificar si el mensaje del usuario coincide con alguna canción cargada
    El mensaje del usuario se divide en un array por saltos de linea y se prueba cada item contra la bdd
    Si por lo menos un item coincide, se muestra su imagen y el resto se responde que no se encontraron
    Si ningún item coincide, se asume que el usuario no pidió canciones
    Se añadió manejo de saludos con un array de posibles saludos:
    Si el usuario saluda, CancioNito responde.

--- services.py
+++ services.py
@@ -12,6 +12,13 @@ def obtener_mensaje_whatsapp(message):
 
     return text
 
+def filtrar_number(number):
+    if number[0:3] == "549": 
+        car = "54"
+        num = number[3:]
+        number = (car+num)
+    return number
+
 def enviar_mensaje_whatsapp(data):
     try:
         whatsapp_token = sett.whatsapp_token
@@ -22,11 +29,10 @@ def enviar_mensaje_whatsapp(data):
         
         response = requests.post(url=whatsapp_url, headers=headers, data=data)
         if response.status_code == 200:
-            print("mensaje respondido")
             return 'mensaje enviado'
         else:
             print("El status code es: "+str(response.status_code))
-            print("El mensaje era: "+str(data)) ##ENTRÓ ACÁ
+            print("El mensaje era: "+str(data))
             return 'mensaje no enviado', response.status_code
     except Exception as e:
         return e,403
@@ -46,7 +52,7 @@ def text_message(number, text):
     )
     return data
 
-def image(number, url):
+def image_message(number, url):
     data = json.dumps(
         {
             "messaging_product": "whatsapp",
@@ -62,10 +68,48 @@ def image(number, url):
 
 def administrar_chatbot(text, number, messageId, name):
     text = text.lower() ##mensaje que envió el usuario
-    list = []
+    canciones = [] #lista de IMAGENES de canciones a enviar
+    el_texto_tiene_canciones = False #asumo que por defecto el usuario no pide canciones
 
-    if 'hola' in text:
-        data = text_message(number, "Hola! Mi nombre es Cancionito.")
+    posibles_saludos=['hola', 'alo', 'hi', 'hello', 'ola', 'buenas', 'buen día', 'buen dia']
+    if any(saludo in text for saludo in posibles_saludos):
+        data = text_message(number, "¡Hola! Mi nombre es CancioNito🎵. Pedime una canción o una lista de canciones separadas por saltos de línea :D")
+        enviar_mensaje_whatsapp(data)
+        data = text_message(number,"Por ahora solo tengo las siguientes canciones:\n-Padre amado\n-Tengo paz\n-Jesucristo basta\nPedime la que quieras!💫")
+        enviar_mensaje_whatsapp(data)
     else:
-        data = text_message(number, "No entendí lo que quisiste decir, lo siento!")
-    enviar_mensaje_whatsapp(data)
+        ##CARGO EL JSON DE LA BDD
+        posibles_canciones = {
+            "Padre amado":"https://imgv2-2-f.scribdassets.com/img/document/281897126/original/5ae918e126/1713539600?v=1",
+            "Tengo paz":"https://imgv2-1-f.scribdassets.com/img/document/325877359/original/706763de2c/1712197720?v=1",
+            "Jesucristo basta":"https://imgv2-2-f.scribdassets.com/img/document/467744741/original/88d82f751d/1710639242?v=1"
+        }
+        #SEPARO EL TEXTO POR ESPACIOS PARA VER SI SON CANCIONES
+        texto_separado = text.split("\n")
+        #COMPRUEBO QUE EL TEXTO SEAN CANCIONES
+        for texto in texto_separado: #itero sobre las canciones del usuario
+            coincidence = False
+            for nombre, imagen in posibles_canciones.items():
+                if texto == nombre.lower():
+                    tupla = imagen,True #creo una tupla con la IMAGEN indicando que fue encontrada
+                    canciones.append(tupla) #guardo la tupla para la cancion encontrada
+                    coincidence = True
+                    break
+            if coincidence:
+                el_texto_tiene_canciones = True
+            else: #si el texto del usuario no coincidió con ninguna canción de la bdd
+                tupla = texto, False #creo una tupla con el TITULO indicando que NO fue encontrado
+                canciones.append(tupla) #guardo la tupla de la cancion no encontrada
+
+        #FUNCIONALIDAD PRINCIPAL = SI HAY CANCIONES, MANDO IMAGEN.
+        if el_texto_tiene_canciones:
+            for cancion, encontrada in canciones:
+                if encontrada:
+                    data = image_message(number, cancion) #notar que acá cancion viene de "imagen", que es una URL
+                else: #notar que acá cancion viene de "texto", un string con el nombre de la cancion.
+                    data = text_message(number, f'No se encontraron coincidencias para "{cancion}" prueba escribirla de otra forma!')
+
+                enviar_mensaje_whatsapp(data)
+        else:
+            data = text_message(number, "No entendí :C intentá escribir el nombre de algún corito o alabanza porfis")
+            enviar_mensaje_whatsapp(data) 
